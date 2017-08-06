16f83f1db947a8f3bf479356e536acb82524c225
Tech4lyfe <theotricem@yahoo.com>
2017-08-06 11:14:32 -0400

    added commenting features

--- AngularBlogProject/App/controllers.js
+++ AngularBlogProject/App/controllers.js
@@ -6,16 +6,31 @@ angular.module("app.controllers", ["app.directives"])
                $scope.posts = response.data
            })
 
-       }).controller("singlePostController", function ($scope,$http,$routeParams) {
-           $http.get("data/post.json") .then(function (response) {
+       }).controller("singlePostController", function ($scope, $http, $routeParams) {
+           $http.get("data/post.json").then(function (response) {
                $scope.post = response.data[$routeParams.id];
            })
-       }).controller("pageController", function ($scope,$http,$routeParams) {
-          
+       }).controller("pageController", function ($scope, $http, $routeParams) {
+
 
            $http.get("data/pages.json").then(function (response) {
                $scope.page = response.data[$routeParams.id];
            })
+       }).controller("commentController", function ($scope) {
+
+           $scope.submit = function () {
+               $scope.comments = [];
+               $scope.comments.push({
+                   name: $scope.name,
+                   text: $scope.text
+               });
+               $scope.name = "";
+               return $scope.text = "";
+           };
+           return $scope.drop = function (comment) {
+               return $scope.comments.splice($scope.comments.indexOf(comment), 1);
+           };
+         
        })
 
 
